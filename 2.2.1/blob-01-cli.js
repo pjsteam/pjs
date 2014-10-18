5f851012727a5fb05241ed8149b9adc67ea199fb
Matias Servetto <mservetto@despegar.com>
2014-10-18 11:57:46 -0300

    [2.2.1] adds initial investigation.

---
+++ 2.2.1/blob-01-cli.js
@@ -0,0 +1,33 @@
+var run = (function () {
+  var runCount = 0;
+  return function (ww) {
+    var elements = [1, 2, 3, 4];
+    var mapper = function (element) {
+      return element + '!';
+    };
+
+    var blob = new Blob(['__f = ' + mapper.toString()], { type: 'application/javascript' });
+    var blobURL = window.URL.createObjectURL(blob);
+
+    runCount += 1;
+    var div = document.getElementById("resultsDiv");
+    while (div.hasChildNodes()) {
+      div.removeChild(div.lastChild);
+    }
+    var p = document.createElement('p');
+    p.textContent = 'Run : ' + runCount;
+    div.appendChild(p);
+    p = document.createElement('p');
+    p.textContent = 'Elementos a transformar: [' + elements + ']';
+    div.appendChild(p);
+
+    console.log('Elementos a transformar:');
+    console.log(elements);
+
+    ww.postMessage({
+      blobURL: blobURL,
+      elements: elements
+    });
+
+  };
+})();
