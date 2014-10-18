55886553b23de56fd2f1f04abf49f5e3101b1576
Matias Servetto <mservetto@despegar.com>
2014-10-18 15:21:10 -0300

    [2.2.1] adds function constructor with transferable object alternative.

---
+++ 2.2.1/function-03-cli.js
@@ -0,0 +1,55 @@
+var run = (function () {
+  var runCount = 0;
+
+  var showStart = function (elements) {
+    runCount += 1;
+    var div = document.getElementById("resultsDiv");
+    while (div.hasChildNodes()) {
+      div.removeChild(div.lastChild);
+    }
+    var p = document.createElement('p');
+    p.textContent = 'Run : ' + runCount;
+    div.appendChild(p);
+    p = document.createElement('p');
+    p.textContent = 'Elementos a transformar: [' + elements + ']';
+    div.appendChild(p);
+
+    console.log('Elementos a transformar:');
+    console.log(elements);
+  };
+
+  var str2ab = function (str) {
+    var buf = new ArrayBuffer(str.length * 2);
+    var bufView = new Uint16Array(buf);
+    for (var i = 0, strLen = str.length; i < strLen; i++) {
+      bufView[i] = str.charCodeAt(i);
+    }
+    return bufView;
+  };
+
+  return function (ww) {
+    var elements = [1, 2, 3, 4];
+    var mapper = function (element) {
+      return element + '!';
+    };
+
+    var stringMapper = mapper.toString();
+    var match = stringMapper.match(/function[^{]+\{([\s\S]*)\}$/);
+    var body = match[1];
+    var args = /\(([^)]+)/.exec(stringMapper);
+    if (args[1]) {
+        args = args[1].split(/\s*,\s*/);
+    }
+    var arg = args[0];
+    var bufferedBody = str2ab(body);
+
+    showStart(elements);
+
+    ww.postMessage({
+      arg: arg,
+      code: bufferedBody,
+      elements: elements
+    }, [bufferedBody.buffer]);
+
+  };
+})();
