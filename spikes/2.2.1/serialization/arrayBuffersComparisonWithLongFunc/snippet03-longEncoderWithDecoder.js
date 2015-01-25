e03405132ee9dfeefe12d27a5401d46c500b65f4
Matias Servetto <mservetto@despegar.com>
2015-01-25 12:06:45 -0300

    Adds serialization tests with one web worker and long serialized code.

---
+++ spikes/2.2.1/serialization/arrayBuffersComparisonWithLongFunc/snippet03-longEncoderWithDecoder.js
@@ -0,0 +1,31 @@
+var f = function () {
+  var n = 1000;
+  var sum = 0;
+  for (var i = 0; i < n; i++) {
+    sum = i + (n * 2 - (i * 3));
+    sum--;
+  }
+  var counter = ((function () { 
+    var safeCounter = 0;
+    return function () { safeCounter++; return safeCounter;};
+  })());
+  while (800 > counter()) {
+    sum--;
+  }
+  postMessage(sum);
+};
+
+var str2ab = function (str) {
+  var strLength = str.length;
+  var buf = new ArrayBuffer(strLength * 2);
+  var bufView = new Uint16Array(buf);
+  var i = strLength;
+  for (; i--; ) {
+    bufView[i] = str.charCodeAt(i);
+  }
+  return bufView;
+};
+
+var encoder = new TextEncoder();
+var b = encoder.encode(f.toString());
+decoderWorker.postMessage(b, [b.buffer]);
