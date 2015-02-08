8c0b2033846564a9030387c0fc1473b93c85cba6
Matias Servetto <mservetto@despegar.com>
2015-02-08 14:45:37 -0300

    [2.3.2] adds new test and implementation for TypedArray partitioner module.

--- src/typed_array_partitioner.js
+++ src/typed_array_partitioner.js
@@ -16,36 +16,23 @@ Partitioner.prototype.partition = function (array) {
 };
 
 Partitioner.prototype.validateTypedArray = function (array) {
-  if (!array) {
-    throw new errors.InvalidArgumentsError(errors.messages.PARTITIONER_ARGUMENT_IS_NOT_TYPED_ARRAY); 
-  }
-  var temp = array.toString();
-  type = temp.substring('[object '.length, temp.length - 1);
-  switch(type){
-    case 'Uint8Array':
-    case 'Int8Array':
-    case 'Uint8ClampedArray':
-    case 'Uint16Array':
-    case 'Int16Array':
-    case 'Uint32Array':
-    case 'Int32Array':
-    case 'Float32Array':
-    case 'Float64Array':
-      return;
-    default:
-      throw new errors.InvalidArgumentsError('Invalid type: ' + type + '. ' + errors.messages.PARTITIONER_ARGUMENT_IS_NOT_TYPED_ARRAY); 
-  }
+  if (!utils.isTypedArray(array)) {
+    throw new errors.InvalidArgumentsError('Invalid Type: ' + array + '. ' + errors.messages.PARTITIONER_ARGUMENT_IS_NOT_TYPED_ARRAY);
+  };
 };
 
 Partitioner.prototype.doPartition = function (array) {
   var parts = this.parts;
-  var subElementsCount = array.length / parts;
+  var subElementsCount = parseInt(array.length / parts);
   var arrays = [];
   for (var i = 0; i < parts; i++) {
     var begin = i * subElementsCount;
     var end = i * subElementsCount + subElementsCount;
+    if (parts - 1 === i) {
+      end = array.length;
+    }
     var subXs = array.subarray(begin, end);
-    arrays.push(new Uint32Array(subXs));
+    arrays.push(new array.constructor(subXs));
   }
   return arrays;
 };
--- src/utils.js
+++ src/utils.js
@@ -9,3 +9,25 @@ utils.getter = function (obj, name, value) {
     }
   });
 };
+
+utils.isTypedArray = function (array) {
+  if (!array) {
+    return false;
+  }
+  var temp = array.toString();
+  type = temp.substring('[object '.length, temp.length - 1);
+  switch(type){
+    case 'Uint8Array':
+    case 'Int8Array':
+    case 'Uint8ClampedArray':
+    case 'Uint16Array':
+    case 'Int16Array':
+    case 'Uint32Array':
+    case 'Int32Array':
+    case 'Float32Array':
+    case 'Float64Array':
+      return true;
+    default:
+      return false;
+  }
+};
