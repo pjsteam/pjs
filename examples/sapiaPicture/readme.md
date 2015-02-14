7762ee490d70b55bf9d021c72ed866948eca2589
dschenkelman <damian.schenkelman@gmail.com>
2015-02-14 20:09:03 -0300

    Added sapia picture sample

---
+++ examples/sapiaPicture/readme.md
@@ -0,0 +1,4 @@
+The idea and some source code for this sample was taken from here:
+http://blogs.msdn.com/b/eternalcoding/archive/2012/09/20/using-web-workers-to-improve-performance-of-image-manipulation.aspx
+
+To run the sample you must serve the **index.html** file using a local server. Otherwise a CORS error is thrown when manipulating the image inside the canvas.
--- src/job_packager.js
+++ src/job_packager.js
@@ -2,6 +2,8 @@ var errors = require('./errors.js');
 var utils = require('./utils.js');
 var Partitioner = require('./typed_array_partitioner.js');
 
+var FUNCTION_REGEX = /^function[^(]*\(([^)]*)\)[^{]*\{([\s\S]*)\}$/
+
 var JobPackager = module.exports = function (parts, code, elements) {
   if (!parts) {
     throw new errors.InvalidArgumentsError(errors.messages.INVALID_PARTS);
@@ -18,9 +20,8 @@ var JobPackager = module.exports = function (parts, code, elements) {
 };
 
 JobPackager.prototype.generatePackages = function () {
-  var reg = /function[^(]+\(([^)]+)\)[^{]\{([\s\S]*)\}/
   var functionString = this.code.toString();
-  var match = functionString.match(reg);
+  var match = functionString.match(FUNCTION_REGEX);
   var packageCodeArg = match[1].split(',')[0].trim();
   var packageCode = match[2];
   var elementsType = utils.getTypedArrayType(this.elements);
