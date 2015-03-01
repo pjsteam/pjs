09446cf6ac86fbd5ca9f544d58e0983dade8e86a
Matias Servetto <mservetto@despegar.com>
2015-03-01 20:13:29 -0300

    [3.2.1] adds pjs.filter tests

--- test/filter.tests.js
+++ test/filter.tests.js
@@ -34,5 +34,51 @@ describe('filter tests', function(){
         });
       });
 
+      var emptySourceArray = new TypedArray([]);
+      it(utils.format('should return no elements for empty array of type {0} in callback', utils.getTypedArrayType(emptySourceArray)), function(done){
+        pjs(emptySourceArray).map(function(e){
+          return true;
+        }, function(result){
+          expect(result).to.have.length(0);
+          expect(utils.getTypedArrayType(result)).to.equal(utils.getTypedArrayType(emptySourceArray));
+          done();
+        });
+      });
+
+      it(utils.format('should return no elements if no elements match predicate for array of type {0} in callback', utils.getTypedArrayType(sourceArray)), function(done){
+        pjs(sourceArray).filter(function(e){
+          return false;
+        }, function(result) {
+          expect(result).to.have.length(0);
+          expect(utils.getTypedArrayType(result)).to.equal(utils.getTypedArrayType(sourceArray));
+          done();
+        });
+      });
+
+      it(utils.format('should return all elements if all elements match predicate for array of type {0} in callback', utils.getTypedArrayType(sourceArray)), function(done){
+        pjs(sourceArray).filter(function(e){
+          return true;
+        }, function(result) {
+          expect(result).to.have.length(sourceArray.length);
+          expect(utils.getTypedArrayType(result)).to.equal(utils.getTypedArrayType(sourceArray));
+          for (var i = 0; i < sourceArray.length; i += 1) {
+            expect(result[i]).to.equal(sourceArray[i]);
+          }
+          done();
+        });
+      });
+
+
+      var singleElementSourceArray = new TypedArray([4]);
+      it(utils.format('should return filtered elements for single element array of type {0} in callback', utils.getTypedArrayType(singleElementSourceArray)), function(done){
+        pjs(singleElementSourceArray).filter(function(e){
+          return true;
+        }, function(result){
+          expect(result).to.have.length(singleElementSourceArray.length);
+          expect(result[0]).to.equal(singleElementSourceArray[0]);
+          expect(utils.getTypedArrayType(result)).to.equal(utils.getTypedArrayType(singleElementSourceArray));
+          done();
+        });
+      });
     });
 });
