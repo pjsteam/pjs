0c911aae126e1f92bd37ab5a5fea7c526e56fe11
Matias Servetto <mservetto@despegar.com>
2015-03-01 11:58:06 -0300

    [2.1.1] updates 2.1.1 benchmarks

--- spikes/2.1.1/newVsPreExistent.js
+++ spikes/2.1.1/newVsPreExistent.js
@@ -1,51 +1,60 @@
-// http://jsperf.com/worker-cold-start/5
+// http://jsperf.com/worker-cold-start/6
 
-// JavaScript setup
-function setupForPreExistentWorker(){
-  var wCode = function(event){
-    var codeBuffer = event.data;
-    var codeArray = new Uint8Array(codeBuffer);
-    var decoder = new TextDecoder();
-    var code = decoder.decode(codeArray);
+// HTML setup
+<script>
+  var __finish;
 
-    eval('var __f = ' + code);
+  function setupForPreExistentWorker(){
+    var wCode = function(event){
+      var codeBuffer = event.data;
+      var codeArray = new Uint8Array(codeBuffer);
+      var decoder = new TextDecoder();
+      var code = decoder.decode(codeArray);
 
-    __f();
-  };
+      eval('var __f = ' + code);
 
-  var blob = new Blob([
+      __f();
+    };
+
+    var blob = new Blob([
+        "onmessage = " + wCode.toString()]);
+
+    var blobURL = window.URL.createObjectURL(blob);
+
+    return new Worker(blobURL);
+  }
+
+  var encoder = new TextEncoder();
+
+  function postCode(worker, f){
+    var b = encoder.encode(f.toString());
+    worker.postMessage(b.buffer, [b.buffer]);
+  }
+
+  var worker = setupForPreExistentWorker();
+
+  worker.onmessage = function(event){
+    __finish();
+  }
+
+  function setupForNewWorkerEachTime(){
+    var wCode = function(event){
+      postMessage(null);
+    };
+
+    var blob = new Blob([
       "onmessage = " + wCode.toString()]);
 
-  var blobURL = window.URL.createObjectURL(blob);
+    var blobURL = window.URL.createObjectURL(blob);
 
-  return new Worker(blobURL);
-}
+    return new Worker(blobURL);
+  }
+</script>
 
-var encoder = new TextEncoder();
-
-function postCode(worker, f){
-  var b = encoder.encode(f.toString());
-  worker.postMessage(b.buffer, [b.buffer]);
-}
-
-var worker = setupForPreExistentWorker();
-
-worker.onmessage = function(event){
+// JavaScript setup
+__finish = function () {
   deferred.resolve();
-}
-
-function setupForNewWorkerEachTime(){
-  var wCode = function(event){
-    postMessage(null);
-  };
-
-  var blob = new Blob([
-    "onmessage = " + wCode.toString()]);
-
-  var blobURL = window.URL.createObjectURL(blob);
-
-  return new Worker(blobURL);
-}
+};
 
 // Test case 1 - Pre existent worker
 postCode(worker, function() { postMessage(null); });
