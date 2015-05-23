ff6bb5196621ae30620754b19422b4436bcc98b8
Matias Servetto <mservetto@despegar.com>
2015-05-23 11:32:52 -0300

    Refactors queueing tests and adds error on queued operation test.

--- test/queueing_workers_jobs.test.js
+++ test/queueing_workers_jobs.test.js
@@ -22,33 +22,51 @@ describe('queueing workers jobs', function(){
         var sourceArrayA = new TypedArray([1,2,3,4,5,6,7,8,9,10,11,12,13,14]);
         var sourceArrayB = new TypedArray([20,21,22,23,24,25,26,27,28,29,30,31]);
 
-        it('should queue consecutive map operations', function(done){
+        it('should respect chains order', function () {
           var mapperA = function(e) { return e * 2; };
-          var mapperB = function(e) { return e * 4; }
+          var mapperB = function(e) { return e * 4; };
           var chainA = pjs(sourceArrayA).map(mapperA);
           var chainB = pjs(sourceArrayB).map(mapperB);
 
-          chainA.seq(function(err, result) {
-            if (err) { return done(err); }
-            finisher(err, result, sourceArrayA, mapperA);
-          });
-          chainB.seq(function(err, result) {
-            if (err) { return done(err); }
-            finisher(err, result, sourceArrayB, mapperB);
-          });
+          var promiseA = chainA.seq();
+          var promiseB = chainB.seq();
 
-          var finishCount = 0;
-          var finisher = function (err, result, sourceArray, mapper) {
-            finishCount++;
-            expect(result).to.have.length(sourceArray.length);
-            expect(utils.getTypedArrayType(result)).to.equal(utils.getTypedArrayType(sourceArray));
-            for (var i = sourceArray.length - 1; i >= 0; i--) {
-              expect(result[i]).to.equal(mapper(sourceArray[i]));
+          return Promise.race([promiseA, promiseB]).then(function (result) {
+            expect(result).to.have.length(sourceArrayA.length);
+            expect(utils.getTypedArrayType(result)).to.equal(utils.getTypedArrayType(sourceArrayA));
+            for (var i = sourceArrayA.length - 1; i >= 0; i--) {
+              expect(result[i]).to.equal(mapperA(sourceArrayA[i]));
             }
-            if (finishCount === 2) {
-              done();
-            }
-          }
+            return promiseB.then(function (result) {
+              expect(result).to.have.length(sourceArrayB.length);
+              expect(utils.getTypedArrayType(result)).to.equal(utils.getTypedArrayType(sourceArrayB));
+              for (var i = sourceArrayB.length - 1; i >= 0; i--) {
+                expect(result[i]).to.equal(mapperB(sourceArrayB[i]));
+              }
+            });
+          });
+        });
+
+        it('should not stop calculation with on invalid seq', function () {
+          var mapperA = function(e, ctx) { return ctx.aux() + e * 2; };
+          var mapperB = function(e) { return e * 4; };
+          var chainA = pjs(sourceArrayA).map(mapperA);
+          var chainB = pjs(sourceArrayB).map(mapperB);
+
+          var promiseA = chainA.seq();
+          var promiseB = chainB.seq();
+
+          return Promise.race([promiseA, promiseB]).catch(function (err) {
+            expect(err.name).to.equal('WorkerError');
+            expect(err.message).to.match(/(Uncaught TypeError: undefined is not a function)|(TypeError: ctx.aux is not a function)/);
+            return promiseB.then(function (result) {
+              expect(result).to.have.length(sourceArrayB.length);
+              expect(utils.getTypedArrayType(result)).to.equal(utils.getTypedArrayType(sourceArrayB));
+              for (var i = sourceArrayB.length - 1; i >= 0; i--) {
+                expect(result[i]).to.equal(mapperB(sourceArrayB[i]));
+              }
+            });
+          });
         });
       });
     });
